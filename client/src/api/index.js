a22f5857f81bed8a1ff1dc04fa67218329c89134
HirdeshGupta <hirdeshgupta16@gmail.com>
2023-01-09 23:56:11 +0530

    doneee

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,12 +1,11 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: process.env.BACKEND_URL });
+const API = axios.create({ baseURL: 'https://mern-auth-eta.vercel.app/'});
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
     req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
   }
-
   return req;
 });
 
